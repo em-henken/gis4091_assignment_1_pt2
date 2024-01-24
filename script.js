6e6f506da5459d7734185f928110c56bb92b9760
em-henken <118228404+em-henken@users.noreply.github.com>
2024-01-24 14:16:52 -0600

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
   // load GeoJSON from an external file
 $.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson", function (data) {
+  console.log(data)
   var ratIcon = L.icon({
     iconUrl: "http://maptimeboston.github.io/leaflet-intro/rat.gif",
     iconSize: [50, 40]
