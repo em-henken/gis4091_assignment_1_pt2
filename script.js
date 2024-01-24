34d43c85dad1b228192fee289002bb3f7efb31eb
em-henken <118228404+em-henken@users.noreply.github.com>
2024-01-24 14:11:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-var map = L.map('map').setView([38.635041,-90.237474], 12);
- L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.{ext}', {
+var map = L.map('map').setView([37.7, -122.4], 10);
+
+  // load a tile layer
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 	subdomains: 'abcd',
 	minZoom: 0,
@@ -7,19 +9,16 @@ var map = L.map('map').setView([38.635041,-90.237474], 12);
 	ext: 'png'
 }).addTo(map);
 
-// load GeoJSON from an external file
-$.getJSON("https://raw.githubusercontent.com/gbrunner/adv-python-for-gis-and-rs/master/Week%201/sf_crime.geojson",function(data){
-// add GeoJSON layer to the map once the file is loaded ???????????????
-L.geoJson(data).addTo(map);
-});
+  // load GeoJSON from an external file
+$.getJSON("https://raw.githubusercontent.com/orhuna/WebGIS_SLU_M1/main/Module%201/Assignment%201/data/sf_crime.geojson", function (data) {
+  var ratIcon = L.icon({
+    iconUrl: "http://maptimeboston.github.io/leaflet-intro/rat.gif",
+    iconSize: [50, 40]
+  });
 
-
-var ratIcon = L.icon({
-    iconUrl: 'http://maptimeboston.github.io/leaflet-intro/rat.gif',
-    iconSize: [50,40]
-}); 
-L.geoJson(data ,{
-    pointToLayer: function(feature,latlng){
-	  return L.marker(latlng,{icon: ratIcon});
+  L.geoJson(data, {
+    pointToLayer: function (feature, latlng) {
+      return L.marker(latlng, { icon: ratIcon });
     }
+  }).addTo(map);
 });
